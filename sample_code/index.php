05bf595eaa86ea64728a7c0ac5554183603a9ab6
Darin Burleigh <darin.burleigh@cdw.com>
2018-02-09 15:18:28 -0600

    .

--- sample_code/index.php
+++ sample_code/index.php
@@ -4,16 +4,18 @@
 <body>
 <h2>Try</h2>
 <ul>
-	<li class=hidden><a href="test_auth.php">App Authentication<a></li>
-		<li><a href="listRooms.php">List Rooms<a></li>
-		<li><a href="listPeople.php">List People<a></li>
-		<li><a href="postMessage.php">Post message in room<a></li>
-		<li><a href="showThread.php">Show Thread<a></li>
+	<li class="hidden"> <a href="test_auth.php">App Authentication</a></li>
+		<li><a href="listRooms.php">List Rooms</a></li>
+		<li><a href="listPeople.php">List People</a></li>
+		<li><a href="postMessage.php">Post message in room</a></li>
+		<li><a href="showThread.php">Show Thread</a></li>
+		<li><a href="lookupUser.php">Lookup user</a></li>
 </ul>
 
 <h3>Reference</h3>
 <ul>
-<li> <a target=spark href="https://web.ciscospark.com">Cisco Spark rooms</></li>
+<li> <a target="spark" href="https://web.ciscospark.com">Cisco Spark rooms </a></li>
+
 <li> <a target=ref href="https://developer.ciscospark.com/support.html" >Spark Dev site</a></li>
 <li> <a target=ref href="https://help.webex.com/community/spark" >help</a></li>
 <li> <a target=ref href="https://status.ciscospark.com/" >Status</a></li>
